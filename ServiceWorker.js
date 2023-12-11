95885200dacb798ca155d2f460e79f61336e7332
a12204002-RyujiAtsuta <a12204002@zokei.ac.jp>
2023-12-11 15:39:05 +0900

    0.5.6_WebGL
    Web動作のテスト

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,8 +1,8 @@
-const cacheName = "AtsutaRyuji-New_Social_Game-0.5.5_WebGL";
+const cacheName = "AtsutaRyuji-New_Social_Game-0.5.6_WebGL";
 const contentToCache = [
     "Build/NewGamPlyaer_AnimationBasicD.loader.js",
     "Build/6f0e6a6228d6dcc942060f2bfb824b0c.js",
-    "Build/808f464b974958fd0e735d91bb1a5cac.data",
+    "Build/2b6cb9050827efaa17e1e80afb8e8276.data",
     "Build/37688cfd5942c7119e17ac4860519e43.wasm",
     "TemplateData/style.css"
 
